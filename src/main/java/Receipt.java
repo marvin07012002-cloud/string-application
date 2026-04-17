0eb337f0d412f20240d019a4829d151051bfa02c
marbin701 <marvin07012002@gmail.com>
2026-04-17 16:49:55 -0400

    Initial commit

---
+++ src/main/java/Receipt.java
@@ -0,0 +1,11 @@
+public class reciept {
+
+    public static void main(String[] args) {
+        String item = "watermelon";
+        double price = 4.99;
+        int numberOfFruits = 5;
+
+        System.out.println("You bought " + numberOfFruits + " " + item + " for " + price);
+
+    }
+}
